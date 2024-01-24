a0da95c6c74adf8e1c70b6a7ce035c1b255929f8
aikooo7 <prozinhopro1973@gmail.com>
2024-01-24 22:36:37 +0000

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+import CenteredTitle from "../components/CenteredTitle";
+
+export default function About() {
+  return (
+    <main>
+      <CenteredTitle>About me</CenteredTitle>
+      <div className="h-screen flex items-center">
+        <div className="mx-auto w-1/2 text-center">
+          <p className="text-xl">
+            I am a hobbiest programmer, who enjoys doing nerdy stuff aka low
+            level and embedded stuff.
+            <br></br>
+            Feel free to <a href="/contact">contact me</a> if you have any
+            questions. <br></br>
+            You can see my work at <a href="/projects">the projects tab</a> or
+            if you want to see all the stuff I do you can see it at{" "}
+            <a href="https://github.com/aikooo7">my github</a>.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
