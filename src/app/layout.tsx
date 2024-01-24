74e4b3f0d4cf8ce51bcc8ff883947285be0d0174
aikooo7 <prozinhopro1973@gmail.com>
2024-01-24 22:31:19 +0000

    feat: add navbar and footer (credits)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { Github } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,42 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="bg-stone-700 text-white">
+        <div className="h-screen">
+          <nav>
+            <div className="font-bold mx-2 absolute top-4">
+              <Link
+                href="/"
+                className="float-left text-center py-2 px-4 text-xl"
+              >
+                Home
+              </Link>
+              <Link
+                href="/about"
+                className="float-left text-center py-2 px-4 text-xl"
+              >
+                About
+              </Link>
+              <Link
+                href="/contact"
+                className="float-left text-center py-2 px-4 text-xl"
+              >
+                Contact
+              </Link>
+            </div>
+          </nav>
+          {children}
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-3">
+            <p className="flex items-center self-end flex-1 gap-2">
+              Done by aikooo7
+              <a href="https://github.com/aikooo7" target="_blank">
+                <Github />
+              </a>
+            </p>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
