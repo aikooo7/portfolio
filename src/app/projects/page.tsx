d58c89733453f4766eeb784270c29e3e988ed74e
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 18:35:25 +0000

    chore: remove duplicate items-center css propriety

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,6 @@ export default function Projects() {
     <main>
       <CenteredTitle>Projects</CenteredTitle>
 
-      <div className="w-screen h-screen flex items-center items-center justify-center">
           <h1 className="text-3xl font-bold">WORK IN PROGRESS...</h1>
         <div className="w-3/4 text-center">
         </div>
