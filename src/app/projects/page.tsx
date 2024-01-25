8be3905adc891381458e8856950c6c81e055faf8
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 18:35:54 +0000

    improvement: remove WORK IN PROGRESS

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,6 @@ export default function Projects() {
     <main>
       <CenteredTitle>Projects</CenteredTitle>
 
-          <h1 className="text-3xl font-bold">WORK IN PROGRESS...</h1>
         <div className="w-3/4 text-center">
         </div>
       </div>
