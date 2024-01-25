41fafb6e7747ab62dba8f8aad7ef946017d81d8f
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 00:22:12 +0000

    impr: make the projects route have a indication that it is work in progress

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,12 @@ export default function Projects() {
   return (
     <main>
       <CenteredTitle>Projects</CenteredTitle>
+
+      <div className="w-screen h-screen flex items-center items-center justify-center">
+        <div className="mx-auto w-1/2 text-center">
+          <h1 className="text-3xl font-bold">WORK IN PROGRESS...</h1>
+        </div>
+      </div>
     </main>
   );
 }
