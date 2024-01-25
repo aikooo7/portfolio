4a4ff1675b01b50ab30740ad0392eb2f93b0acf8
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 18:36:35 +0000

    feat: add div that occupies all the screen

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,7 @@ export default function Projects() {
     <main>
       <CenteredTitle>Projects</CenteredTitle>
 
+      <div className="w-screen h-screen flex items-center justify-center">
         <div className="w-3/4 text-center">
         </div>
       </div>
