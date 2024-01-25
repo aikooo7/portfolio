6dcc71a49d9e93c8bacdf8ed796d6b843b2d9a9b
aikooo7 <prozinhopro1973@gmail.com>
2024-01-25 00:14:27 +0000

    fix: escape "'" with "&apos;"

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
           <p>
             You can contact me by discord (aikoo7), by matrix (@aikoo7:matrix.org) or to my <a href="mailto:prozinhopro1973@gmail.com">email</a>
             <br></br>
-            If you have any security concern or just want a more secure way of communication, send the text cryptographed with <a target="_blank" href="https://keys.openpgp.org/search?q=B0D72955235F6AB5ACFA16198C7FF5BB1ADEF191">my gpg key</a>, and (optionally) if you want me to use yours too, send it in the message and also the message needs to be signed with that key else I'm going to assume that someone is trying to impersonate you.
+            If you have any security concern or just want a more secure way of communication, send the text cryptographed with <a target="_blank" href="https://keys.openpgp.org/search?q=B0D72955235F6AB5ACFA16198C7FF5BB1ADEF191">my gpg key</a>, and (optionally) if you want me to use yours too, send it in the message and also the message needs to be signed with that key else I&apos;m going to assume that someone is trying to impersonate you.
           </p>
         </div>
       </div>
